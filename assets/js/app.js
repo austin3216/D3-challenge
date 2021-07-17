6a2cbebba7ecb8f38d22b0ba04b69b28f02e6e28
austin3216 <aroyster@pm.me>
2021-07-17 11:40:06 -0600

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
 // @TODO: YOUR CODE HERE!
+
+// Create SVG size and attributes
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// SVG wrapper, append SVG group to hold chart
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
