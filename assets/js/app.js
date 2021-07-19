f4ff2a3602cefea51da4e2285ce65daabcf71cd1
austin3216 <aroyster@pm.me>
2021-07-19 08:14:44 -0600

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -83,10 +83,59 @@ let selYAxis = "healthcare";
         .attr("dy", d => yLinearScale(d[selYAxis]))
         .classed("circleText", true);
    
+    // Create x axis labels (poverty, age, income; poverty is initial)
 
+    const xAxisLabels = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height})`);
 
+    const povertyXAxis = xAxisLabels.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "poverty")
+        .text("Poverty Level (%)")
+        .classed("active", true);
 
+    const ageXAxis = xAxisLabels.append("text")
+        .attr("x", 0)
+        .attr("y", 60)
+        .attr("value", "age")
+        .text("Median Age")
+        .classed("inactive", true);
 
+    const incomeXAxis = xAxisLabels.append("text")
+        .attr("x", 0)
+        .attr("y", 80)
+        .attr("value", "income")
+        .text("Median Household Income")
+        .classed("inactive", true);
+
+    // Create y axis labels (healthcare, smoking, obesity; healthcare is initial)
+
+    const yAxisLabels = chartGroup.append("g");
+
+    const healthcareYAxis = yAxisLabels.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2))
+      .attr("y", -40)
+      .attr("value", "healthcare")
+      .text("% Without Healthcare")
+      .classed("active", true);
+  
+    const smokeYAxis = yAxisLabels.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2))
+      .attr("y", -60)
+      .attr("value", "smokes")
+      .text("% Smokers")
+      .classed("inactive", true);
+  
+    const obesityYAxis = yAxisLabels.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2))
+      .attr("y", -80)
+      .attr("value", "obesity")
+      .text("Obesity Level (%)")
+      .classed("inactive", true);
 
 
 })();
