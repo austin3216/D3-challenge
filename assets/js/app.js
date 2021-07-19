2fd7f75a21c63847c50dc33725ef516820f072d8
austin3216 <aroyster@pm.me>
2021-07-19 10:03:41 -0600

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,30 +3,6 @@
 // NOTE -> working with var vs const vs let https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/
 // Switching back to var until I know more about this...
 
-// Create SVG size and attributes
-
-var svgWidth = 960;
-var svgHeight = 500;
-
-var margin = {
-    top: 20,
-    right: 40,
-    bottom: 60,
-    left: 100
-};
-
-var chartWidth = svgWidth - margin.left - margin.right;
-var chartHeight = svgHeight - margin.top - margin.bottom;
-
-// SVG wrapper, append SVG group to hold chart
-var svg = d3.select("#scatter")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
-
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 // Set initial axis parameters
 var selXAxis = "poverty";
 var selYAxis = "healthcare";
@@ -129,36 +105,75 @@ function updateToolTip(selXAxis, selYAxis, circleGroup, circleGroupText) {
     return circleGroup;
 };
 
+// Function to create chart and make responsive
 
+function responsiveChart() {
+
+    var svgWidth = 960;
+    var svgHeight = 500;
+    
+    var margin = {
+        top: 20,
+        right: 40,
+        bottom: 60,
+        left: 100
+    };
+    
+    var chartWidth = svgWidth - margin.left - margin.right;
+    var chartHeight = svgHeight - margin.top - margin.bottom;
+    
+    // SVG wrapper, append SVG group to hold chart
+    var svg = d3.select("#scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+    
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    d3.csv("assets/data/data.csv").then(function(csvData, err) {
+        if (err) throw err;
+        // Parse data to return numbers
+        csvData.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+            data.age = +data.age;
+            data.smokes = +data.smokes;
+            data.obesity = +data.obesity;
+            data.income = +data.income;   
+        });
+
+        // Create x and y scales 
+        var xLinearScale = xScale(csvData, selXAxis, chartWidth);
+        var yLinearScale = yScale(csvData, selYAxis, chartHeight);
+
+        // Create axis functions
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+
+        // Append axes to chart
+        var xAxis = chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(bottomAxis);
+    
+        var yAxis = chartGroup.append("g").call(leftAxis);
+
+    })  
+
+}
 
 
 // // Import CSV data
-//     const csvData = await d3.csv("assets/data/data.csv");
+//     
     
-//     // Parse data to return numbers
-//     csvData.forEach(function(data) {
-//         data.poverty = +data.poverty;
-//         data.healthcare = +data.healthcare;
-//         data.age = +data.age;
-//         data.smokes = +data.smokes;
-//         data.obesity = +data.obesity;
-//         data.income = +data.income;   
-//     });
+//     
+//     
 
-//     // Create scale functions  
-//     let xLinearScale = xScale(csvData, selXAxis);
-//     let yLinearScale = yScale(csvData, selYAxis);
+//     
 
-//     // Create axis functions
-//     let bottomAxis = d3.axisBottom(xLinearScale);
-//     let leftAxis = d3.axisLeft(yLinearScale);
+//     
 
-//      // Append axes to chart
-//     let xAxis = chartGroup.append("g")
-//         .attr("transform", `translate(0, ${height})`)
-//         .call(bottomAxis);
-    
-//     let yAxis = chartGroup.append("g").call(leftAxis);
+
 
 //     // Create data circles
     
